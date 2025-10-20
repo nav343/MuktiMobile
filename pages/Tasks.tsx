67a8e69d713c5dd0c6097635c166ebe67a718238
nav343 <snmlogic111@gmail.com>
2025-10-20 10:24:36 +0530

    Added prompt for redoing deleted task

--- pages/Tasks.tsx
+++ pages/Tasks.tsx
@@ -1,16 +1,17 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Theme } from "../utils/themes/Theme";
-import { Alert, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import {
   ArrowLeft,
   CircleCheckBig,
   Circle,
   Plus,
   Trash2,
+  CircleQuestionMark,
 } from "lucide-react-native";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 interface TaskData {
   title: string;
@@ -24,6 +25,7 @@ export default function Tasks(props: Theme) {
   const { colors } = props;
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   const [selectedId, setSelectedId] = useState<number>(-1);
+  const [confirmation, setConfirmation] = useState<boolean>(false);
   const [lastDeleted, setLastDeleted] = useState<any>();
 
   function changeState(prev: TaskData[], taskId: number): TaskData[] {
@@ -40,21 +42,6 @@ export default function Tasks(props: Theme) {
     setLastDeleted(prev.splice(taskId, 1)[0]);
     return prev;
   }
-  /**
-  useEffect(() => {
-    Alert.alert("Last Chance", "Task Deleted. Do you want to redo?", [
-      {
-        text: "No",
-        onPress: () => console.log("He choose not to save it dam"),
-        style: "cancel",
-      },
-      {
-        text: "Yes",
-        onPress: () => setTasks((prev) => [...prev, lastDeleted]),
-      },
-    ]);
-  }, [lastDeleted]);
-  **/
   const [tasks, setTasks] = useState<TaskData[]>([
     {
       title: "Task 1",
@@ -88,23 +75,104 @@ export default function Tasks(props: Theme) {
   ]);
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: colors.bg,
-        padding: 30,
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <View
+    <>
+      {confirmation == true ? (
+        <View
+          style={{
+            backgroundColor: "rgba(0,0,0,0.5)",
+            position: "absolute",
+            zIndex: 10000,
+            top: 0,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: colors.bg_inverted,
+              width: "70%",
+              padding: 20,
+              paddingVertical: 40,
+              borderRadius: 40,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <CircleQuestionMark
+              color={"#DC2525"}
+              size={55}
+              style={{ marginBottom: 10 }}
+            />
+            <Text
+              style={{
+                color: colors.text_inverted,
+                fontSize: 28,
+                fontWeight: 600,
+                textAlign: "center",
+              }}
+            >
+              Task Deleted
+            </Text>
+            <Text style={{ color: "gray", fontSize: 22, marginTop: 5 }}>
+              Do you want to redo?
+            </Text>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around",
+                width: "70%",
+                marginTop: 10,
+              }}
+            >
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => {
+                  setTasks((prev) => [...prev, lastDeleted]);
+                  setConfirmation(!confirmation);
+                }}
+                style={{
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  borderColor: colors.bg,
+                  borderWidth: 1,
+                  borderRadius: 20,
+                }}
+              >
+                <Text style={{ color: colors.text_inverted, fontSize: 18 }}>
+                  Yes
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => setConfirmation(!confirmation)}
+                style={{
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  backgroundColor: "#DC2525",
+                  borderRadius: 20,
+                }}
+              >
+                <Text style={{ color: colors.text, fontSize: 18 }}>No</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      ) : (
+        <></>
+      )}
+      <SafeAreaView
         style={{
+          flex: 1,
+          backgroundColor: colors.bg,
+          padding: 30,
           display: "flex",
-          flexDirection: "row",
           alignItems: "center",
-          justifyContent: "space-between",
-          width: "100%",
-          marginBottom: 20,
         }}
       >
         <View
@@ -112,141 +180,153 @@ export default function Tasks(props: Theme) {
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            marginBottom: 20,
           }}
         >
-          <ArrowLeft
-            color={colors.text}
-            fontSize={24}
-            style={{ marginRight: 10 }}
-            onPress={() => navigation.goBack()}
-          />
-          <Text style={{ color: colors.text, fontSize: 38, fontWeight: 600 }}>
-            Tasks
-          </Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <ArrowLeft
+              color={colors.text}
+              fontSize={24}
+              style={{ marginRight: 10 }}
+              onPress={() => navigation.goBack()}
+            />
+            <Text style={{ color: colors.text, fontSize: 38, fontWeight: 600 }}>
+              Tasks
+            </Text>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => navigation.navigate("createTask")}
+            style={{
+              padding: 10,
+              borderWidth: 2,
+              borderRadius: 999,
+              borderColor: "gray",
+              marginRight: 10,
+            }}
+          >
+            <Plus color={colors.text} width={24} height={24} />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={() => navigation.navigate("createTask")}
-          style={{
-            padding: 10,
-            borderWidth: 2,
-            borderRadius: 999,
-            borderColor: "gray",
-            marginRight: 10,
-          }}
-        >
-          <Plus color={colors.text} width={24} height={24} />
-        </TouchableOpacity>
-      </View>
 
-      {tasks.length != 0 ? (
-        <ScrollView style={{ width: "100%", paddingVertical: 20 }}>
-          {tasks.map((data, id) => (
-            <TouchableOpacity
-              onLongPress={() => setSelectedId(id)}
-              key={id}
-              activeOpacity={0.7}
-              onPress={() => {
-                setTasks(changeState(tasks, id));
-                setSelectedId(-1);
-              }}
-              style={{
-                backgroundColor: selectedId == id ? "#3B9797" : colors.tile,
-                width: "100%",
-                padding: 30,
-                borderRadius: 20,
-                borderLeftWidth: 4,
-                borderLeftColor: data.color,
-                marginVertical: 10,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                flexDirection: "row",
-                opacity: data.completed ? 0.6 : 1,
-              }}
-            >
-              {data.completed ? (
-                <CircleCheckBig color={colors.text} size={28} />
-              ) : (
-                <Circle color={colors.text} size={28} />
-              )}
-              <View style={{ width: "90%" }}>
-                <Text
-                  style={{
-                    color: data.color,
-                    fontSize: 25,
-                    fontWeight: 600,
-                    textDecorationLine: data.completed
-                      ? "line-through"
-                      : "none",
-                  }}
-                >
-                  {data.title}
-                </Text>
-                <Text
-                  style={{
-                    color: colors.text,
-                    fontSize: 18,
-                    marginVertical: 5,
-                  }}
-                >
-                  {data.desc}
-                </Text>
-                {data.additional != undefined ? (
+        {tasks.length != 0 ? (
+          <ScrollView style={{ width: "100%", paddingVertical: 20 }}>
+            {tasks.map((data, id) => (
+              <TouchableOpacity
+                onLongPress={() => setSelectedId(id)}
+                key={id}
+                activeOpacity={0.7}
+                onPress={() => {
+                  setTasks(changeState(tasks, id));
+                  setSelectedId(-1);
+                }}
+                style={{
+                  backgroundColor: selectedId == id ? "#3B9797" : colors.tile,
+                  width: "100%",
+                  padding: 30,
+                  borderRadius: 20,
+                  borderLeftWidth: 4,
+                  borderLeftColor: data.color,
+                  marginVertical: 10,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  flexDirection: "row",
+                  opacity: data.completed ? 0.6 : 1,
+                }}
+              >
+                {data.completed ? (
+                  <CircleCheckBig color={colors.text} size={28} />
+                ) : (
+                  <Circle color={colors.text} size={28} />
+                )}
+                <View style={{ width: "90%" }}>
+                  <Text
+                    style={{
+                      color: data.color,
+                      fontSize: 25,
+                      fontWeight: 600,
+                      textDecorationLine: data.completed
+                        ? "line-through"
+                        : "none",
+                    }}
+                  >
+                    {data.title}
+                  </Text>
                   <Text
                     style={{
                       color: colors.text,
-                      fontSize: 14,
-                      fontStyle: "italic",
+                      fontSize: 18,
+                      marginVertical: 5,
                     }}
                   >
-                    {data.additional}{" "}
+                    {data.desc}
                   </Text>
-                ) : (
-                  <></>
-                )}
-              </View>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      ) : (
-        <View
-          style={{
-            flex: 1,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ color: colors.text, fontSize: 38, fontWeight: 700 }}>
-            You don't have any Tasks!
-          </Text>
-        </View>
-      )}
-      {selectedId != -1 ? (
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={() => {
-            setSelectedId(-1);
-            setTasks(deleteTask(tasks, selectedId));
-          }}
-          style={{
-            position: "absolute",
-            bottom: "10%",
-            backgroundColor: colors.bg_inverted,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingHorizontal: 30,
-            borderRadius: 99,
-            paddingVertical: 20,
-          }}
-        >
-          <Trash2 color={"#DC2525"} size={38} />
-        </TouchableOpacity>
-      ) : (
-        <></>
-      )}
-    </SafeAreaView>
+                  {data.additional != undefined ? (
+                    <Text
+                      style={{
+                        color: colors.text,
+                        fontSize: 14,
+                        fontStyle: "italic",
+                      }}
+                    >
+                      {data.additional}{" "}
+                    </Text>
+                  ) : (
+                    <></>
+                  )}
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        ) : (
+          <View
+            style={{
+              flex: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: colors.text, fontSize: 38, fontWeight: 700 }}>
+              You don't have any Tasks!
+            </Text>
+          </View>
+        )}
+        {selectedId != -1 ? (
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {
+              setSelectedId(-1);
+              setTasks(deleteTask(tasks, selectedId));
+              setConfirmation(!confirmation);
+            }}
+            style={{
+              position: "absolute",
+              bottom: "10%",
+              backgroundColor: colors.bg_inverted,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingHorizontal: 30,
+              borderRadius: 99,
+              paddingVertical: 20,
+            }}
+          >
+            <Trash2 color={"#DC2525"} size={38} />
+          </TouchableOpacity>
+        ) : (
+          <></>
+        )}
+      </SafeAreaView>
+    </>
   );
 }
